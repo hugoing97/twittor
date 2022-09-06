066897f7b6925b5cf5bcf3e18deeaf94a1fb076c
Hugo Cordova <victor.cordova@mercadolibre.com.mx>
2022-09-06 14:33:50 -0500

    Perfil

---
+++ routers/verPerfil.go
@@ -0,0 +1,28 @@
+package routers
+
+import (
+	"encoding/json"
+	"github.com/hugoing97/twittor/bd"
+	"net/http"
+)
+
+func VerPerfil(w http.ResponseWriter, r *http.Request) {
+
+	ID := r.URL.Query().Get("id")
+
+	if len(ID) < 1 {
+		http.Error(w, "Debe enviar el parametro ID", http.StatusBadRequest)
+		return
+	}
+
+	perfil, err := bd.BuscoPerfil(ID)
+
+	if err != nil {
+		http.Error(w, "Ocurrio un error al intentar buscar el registro"+err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(perfil)
+}
